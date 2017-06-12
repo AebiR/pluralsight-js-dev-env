a24f6607d6bad2580f4c3ec75aa18c8c765c9e72
Ryan Aebi <raebi.shiftdigital@outlook.com>
2017-06-12 15:57:26 -0400

    Mod 11 site API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-
+import {getUsers} from './api/userApi';
 /*
 import numeral from 'numeral';
 
@@ -7,3 +7,20 @@ const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} for this awesome course.`);
 */
+
+
+//Populate table w/ users
+getUsers().then(result => {
+	let usersBody = "";
+
+	result.forEach(user => {
+		usersBody += `<tr>
+		<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+		<td>${user.id}</td>
+		<td>${user.firstName}</td>
+		<td>${user.lastName}</td>
+		<td>${user.email}</td>
+		</tr>`
+	});
+	global.document.getElementById('users').innerHTML = usersBody;
+});
